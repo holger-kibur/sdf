a0fd8ae1b8f154f845e7acf86ae8e918dc70e969
Holger Kibur <holger.kibur@gmail.com>
2022-04-01 09:23:07 -0700

    transfer, more testing stuff

--- sdf/src/node.rs
+++ sdf/src/node.rs
@@ -151,7 +151,7 @@ impl SdfNode {
                 intern.get_info().num_slots(),
                 128
             )),
-            intern: Box::new(intern),
+            intern: intern,
             bbox: None,
         }
     }
@@ -332,7 +332,7 @@ impl SdfNode {
         if self.is_primitive() {
             return NnResult {
                 node: self,
-                distance: self.intern.distance_to(point),
+                distance: self.intern.distance_to(self.bbox.unwrap().in_box_trans_basis(point.extend(1.0)).truncate()),
             };
         }
 
@@ -357,7 +357,7 @@ impl SdfNode {
                         accum
                     } else {
                         let child_nn = node.nearest_neighbor(node.downtree(
-                            node.bbox.unwrap().in_box_trans_basis(point.extend(1.0)).truncate()
+                            self.bbox.unwrap().in_box_trans_basis(point.extend(1.0)).truncate()
                         ));
                         match child_nn.distance < accum.distance {
                             true => child_nn,
@@ -414,20 +414,21 @@ impl SdfBuilder {
     }
 }
 
-#[cfg(test)]
-mod tests {
+#[cfg(test]
+pub mod tests {
     use rand::prelude::*;
-    use std::f32::consts::{PI, FRAC_PI_2};
+    use std::f32::consts::{PI, FRAC_PI_2, SQRT_2};
     use bevy::prelude::*;
     use crate::{
         node::*,
         elements::*,
     };
+    use float_cmp::approx_eq;
 
     #[derive(Debug)]
     struct TestPrimitive {
         pub scale: f32,
-        pub rev_res: u32,
+        pub max_dev: f32,
     }
 
     impl SdfElement for TestPrimitive {
@@ -442,24 +443,24 @@ mod tests {
         fn clone(&self) -> Box<dyn SdfElement> {
             Box::new(TestPrimitive {
                 scale: self.scale,
-                rev_res: self.rev_res,
+                max_dev: self.max_dev,
             })
         }
 
         fn distance_to(&self, point: Vec3) -> f32 {
-            let res_fl = self.rev_res as f32;
+            let sectors = 360.0 / (self.max_dev / SQRT_2);
             let azimuth = f32::atan2(point.z, point.x) + PI;
             let zenith = f32::atan(point.y / f32::sqrt(point.x * point.x + point.y * point.y)) + FRAC_PI_2;
-            let azimuth_quant = (azimuth * res_fl / (2.0 * PI)).trunc();
-            let zenith_quant = (zenith * (res_fl / 2.0) / PI).trunc();
-            let paired = zenith_quant * res_fl + azimuth_quant;
-            point.length() - paired / (res_fl * res_fl / 2.0) * self.scale
+            let azimuth_quant = (azimuth * sectors).trunc();
+            let zenith_quant = (zenith * sectors / 2.0).trunc();
+            let paired = zenith_quant * sectors + azimuth_quant;
+            point.length() - paired / (sectors * sectors / 2.0) * self.scale
         }
 
         fn get_dt_specific_block(&self) -> SdfOpSpecificBlock {
             let mut ret = SdfOpSpecificBlock::ZERO;
             ret.floats[0] = self.scale;
-            ret.floats[1] = self.rev_res as f32;
+            ret.floats[1] = self.max_dev;
             ret
         }
 
@@ -490,6 +491,19 @@ mod tests {
             .collect()
     }
 
+    /**
+     * Re-used test for checking whether a dense, single-element sdf-tree performs nearest neighbor
+     * seach correctly on a given primitive after transformations.
+     * 
+     * Transformations tested are translation, rotation, translation after rotation, and rotation after
+     * translation. For any given primitive, the sdf-tree nearest neighbor results should match the
+     * computed ground truth values.
+     * 
+     * See [`test_dense_nn_single_uni()`] and [`test_dense_nn_single_dir()`] for usage.
+     * 
+     * Generally, this test is meant to check whether [`SdfBuilder::transform()`] and
+     * [`SdfBoundingBox::apply_transform()`] are working as intended.
+     */
     fn do_dense_nn_single(prim: Box<dyn SdfElement>) {
         let mut rng = thread_rng();
         let tlate_trans = Transform::from_translation(Vec3::new(
@@ -497,101 +511,159 @@ mod tests {
             rng.gen_range(-50.0..50.0),
             rng.gen_range(-50.0..50.0),
         ));
+        let tlate_trans_mat = tlate_trans.compute_matrix();
+        let tlate_trans_inv = tlate_trans_mat.inverse();
         let rot_trans = Transform::from_rotation(Quat::from_euler(
             EulerRot::XYZ,
             rng.gen_range(0.0..(2.0 * PI)),
             rng.gen_range(0.0..(2.0 * PI)),
             rng.gen_range(0.0..(2.0 * PI)),
         ));
+        let rot_trans_mat = rot_trans.compute_matrix();
+        let rot_trans_inv = rot_trans_mat.inverse();
         let point = Vec3::new(
             rng.gen_range(-50.0..50.0),
             rng.gen_range(-50.0..50.0),
             rng.gen_range(-50.0..50.0),
         );
 
-        println!("Simple Transform Test Debug:")
+        println!("Simple Transform Test Debug:");
         println!("Sample Point: {}", point);
-        println!("Translation Matrix: {}", tlate_trans.compute_matrix());
-        println!("Rotation Matrix: {}", rot_trans.compute_matrix());
+        println!("Translation Matrix: {}", tlate_trans_mat);
+        println!("Rotation Matrix: {}", rot_trans_mat);
 
-        let gt_tlate = tlate_trans * point;
-        let gt_rot = rot_trans * point;
-        let gt_tlate_rot = rot_trans * tlate_trans * point;
-        let gt_rot_tlate = tlate_trans * rot_trans * point;
+        let gt_tlate = prim.distance_to((tlate_trans_inv * point.extend(1.0)).truncate());
+        let gt_rot = prim.distance_to((rot_trans_inv * point.extend(1.0)).truncate());
+        let gt_tlate_rot = prim.distance_to((tlate_trans_inv * rot_trans_inv * point.extend(1.0)).truncate());
+        let gt_rot_tlate = prim.distance_to((rot_trans_inv * tlate_trans_inv * point.extend(1.0)).truncate());
 
         println!("Translation Ground Truth: {}", gt_tlate);
         println!("Rotation Ground Truth: {}", gt_rot);
-        println!("Translation->Rotation Truth: {}", gt_tlate_rot);
+        println!("Translation->Rotation Ground Truth: {}", gt_tlate_rot);
         println!("Rotation->Translation Ground Truth: {}", gt_rot_tlate);
 
         let tree_tlate = SdfBuilder::dyn_primitive(prim.clone())
             .transform(tlate_trans)
-            .finalize();
+            .finalize()
+            .nearest_neighbor(point)
+            .distance;
         let tree_rot = SdfBuilder::dyn_primitive(prim.clone())
             .transform(rot_trans)
-            .finalize();
-        let tree_tlate_rot
+            .finalize()
+            .nearest_neighbor(point)
+            .distance;
+        let tree_tlate_rot = SdfBuilder::dyn_primitive(prim.clone())
+            .transform(tlate_trans)
+            .transform(rot_trans)
+            .finalize()
+            .nearest_neighbor(point)
+            .distance;
+        let tree_rot_tlate = SdfBuilder::dyn_primitive(prim)
+            .transform(rot_trans)
+            .transform(tlate_trans)
+            .finalize()
+            .nearest_neighbor(point)
+            .distance;
+
+        println!("Translation Tree Result: {}", tree_tlate);
+        println!("Rotation Tree Result: {}", tree_rot);
+        println!("Translation->Rotation Tree Result: {}", tree_tlate_rot);
+        println!("Rotation->Translation Tree Result: {}", tree_rot_tlate);
+
+        assert!(approx_eq!(f32, gt_tlate, tree_tlate),
+            "Simple Translation Failed!");
+        assert!(approx_eq!(f32, gt_rot, tree_rot),
+            "Simple Rotation Failed!");
+        assert!(approx_eq!(f32, gt_tlate_rot, tree_tlate_rot),
+            "Simple Translation->Rotation Failed!"); 
+        assert!(approx_eq!(f32, gt_rot_tlate, tree_rot_tlate),
+            "Simple Rotation->Translation Failed!"); 
     }
 
+    fn do_dense_nn_chain(prim: Box<dyn SdfElement>) {
+        let mut rng = thread_rng();
+        let point = Vec3::new(
+            rng.gen_range(-50.0..50.0),
+            rng.gen_range(-50.0..50.0),
+            rng.gen_range(-50.0..50.0),
+        );
+        let trans_vec = get_random_transforms(10);
+        let trans_inv_vec = trans_vec.iter()
+            .map(|trans| trans.compute_matrix().inverse())
+            .collect();
+    }
+
+    /**
+     * Run a sanity test for whether TestPrimitive actually works as intended.
+     * 
+     * This test is never expected to break or fail; it has no reliance on the rest of the library.
+     * 
+     */
+    #[test]
+    fn sanity_dir_depend_prim() {
+        let prim = TestPrimitive {
+            scale: 10.0,
+            max_dev: 0.1,
+        };
+        assert!(!float_cmp::approx_eq!(
+            f32,
+            prim.distance_to(Vec3::new(50.0, 0.0, 0.0)),
+            prim.distance_to(Vec3::new(49.8097349, 4.35778713, 0.0))
+        ));
+    }
+
+    #[test]
     fn test_dense_nn_single_uni() {
         let prim = SdfSphere {
             radius: 1.0,
         };
-        let mut rng = thread_rng();
-        let tlate_trans = Transform::from_translation(Vec3::new(
-            rng.gen_range(-50.0..50.0),
-            rng.gen_range(-50.0..50.0),
-            rng.gen_range(-50.0..50.0),
-        ));
-        let rot_trans = Transform::from_rotation(Quat::from_euler(
-            EulerRot::XYZ,
-            rng.gen_range(0.0..(2.0 * PI)),
-            rng.gen_range(0.0..(2.0 * PI)),
-            rng.gen_range(0.0..(2.0 * PI)),
-        ));
-        let point = Vec3::new(
-            rng.gen_range(-50.0..50.0),
-            rng.gen_range(-50.0..50.0),
-            rng.gen_range(-50.0..50.0),
-        );
-        let point_translated = 
+        do_dense_nn_single(Box::new(prim));
     }
 
     #[test]
-    fn test_dense_nn_transform_chain() {
+    fn test_dense_nn_single_dir() {
         let prim = TestPrimitive {
-            scale: 10.0,
-            rev_res: 64,
+            scale: 1.0,
+            max_dev: 0.1,
         };
-        let trans_chain = get_random_transforms(1);
-        let point = Vec3::new(
-            thread_rng().gen_range(-50.0..50.0),
-            thread_rng().gen_range(-50.0..50.0),
-            thread_rng().gen_range(-50.0..50.0),
-        );
-        let gt_point = trans_chain.iter()
-            .fold(
-                point,
-                |accum, trans| trans.mul_vec3(accum)
-            );
-        let sdf_tree = trans_chain.iter()
-            .rev()
-            .fold(
-                SdfBuilder::primitive(TestPrimitive {
-                    scale: prim.scale,
-                    rev_res: prim.rev_res,
-                }),
-                |accum, trans| {
-                    accum.transform(*trans).operation(SdfUnion {
-                        smooth_radius: 0.0,
-                    })
-                }
-            )
-            .finalize();
-
-        let ground_truth = prim.distance_to(gt_point);
-        let dense_tree_nn = sdf_tree.nearest_neighbor(point).distance;
-        assert!(float_cmp::approx_eq!(f32, ground_truth, dense_tree_nn, ulps = 2),
-            "Ground Truth: {}, NN Result: {}!", ground_truth, dense_tree_nn);
+        do_dense_nn_single(Box::new(prim));
     }
+
+    // #[test]
+    // fn test_dense_nn_transform_chain() {
+    //     let prim = TestPrimitive {
+    //         scale: 10.0,
+    //         rev_res: 64,
+    //     };
+    //     let trans_chain = get_random_transforms(1);
+    //     let point = Vec3::new(
+    //         thread_rng().gen_range(-50.0..50.0),
+    //         thread_rng().gen_range(-50.0..50.0),
+    //         thread_rng().gen_range(-50.0..50.0),
+    //     );
+    //     let gt_point = trans_chain.iter()
+    //         .fold(
+    //             point,
+    //             |accum, trans| trans.mul_vec3(accum)
+    //         );
+    //     let sdf_tree = trans_chain.iter()
+    //         .rev()
+    //         .fold(
+    //             SdfBuilder::primitive(TestPrimitive {
+    //                 scale: prim.scale,
+    //                 max_dev: prim.max,
+    //             }),
+    //             |accum, trans| {
+    //                 accum.transform(*trans).operation(SdfUnion {
+    //                     smooth_radius: 0.0,
+    //                 })
+    //             }
+    //         )
+    //         .finalize();
+
+    //     let ground_truth = prim.distance_to(gt_point);
+    //     let dense_tree_nn = sdf_tree.nearest_neighbor(point).distance;
+    //     assert!(float_cmp::approx_eq!(f32, ground_truth, dense_tree_nn, ulps = 2),
+    //         "Ground Truth: {}, NN Result: {}!", ground_truth, dense_tree_nn);
+    // }
 }
